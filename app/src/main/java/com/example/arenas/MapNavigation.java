0cca47ad421450f9bec27a7381b7c96f2fb8ae3a
Ezequiel <ezequielmelogno24@gmail.com>
2022-01-04 21:35:22 -0300

    botones anadidos

--- app/src/main/java/com/example/arenas/MapNavigation.java
+++ app/src/main/java/com/example/arenas/MapNavigation.java
@@ -4,12 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
-import android.animation.TimeInterpolator;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
-import android.view.animation.LinearInterpolator;
 import android.widget.Button;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -28,30 +26,107 @@ public class MapNavigation extends AppCompatActivity {
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         //DECLARO BOTON
-        final Button button = findViewById(R.id.button);
+        final Button buttonsolar1 = findViewById(R.id.buttonsolar1);
+        final Button buttonsolar2 = findViewById(R.id.buttonsolar2);
+        final Button buttonsolar3 = findViewById(R.id.buttonsolar3);
+        final Button buttonsolar4 = findViewById(R.id.buttonsolar4);
+        final Button buttonsolar5 = findViewById(R.id.buttonsolar5);
+        final Button buttonsolar6 = findViewById(R.id.buttonsolar6);
+        final Button buttonsolar7 = findViewById(R.id.buttonsolar7);
+        final Button buttonsolar8 = findViewById(R.id.buttonsolar8);
+        final Button buttonsolar9 = findViewById(R.id.buttonsolar9);
+        final Button buttonsolar10 = findViewById(R.id.buttonsolar10);
+        final Button buttonsolar11 = findViewById(R.id.buttonsolar11);
+        final Button buttonsolar12 = findViewById(R.id.buttonsolar12);
+        final Button buttonsolar13 = findViewById(R.id.buttonsolar13);
+        final Button buttonsolar14 = findViewById(R.id.buttonsolar14);
+        final Button buttonsolar15 = findViewById(R.id.buttonsolar15);
+        final Button buttonsolar16 = findViewById(R.id.buttonsolar16);
+        final Button buttonsolar17 = findViewById(R.id.buttonsolar17);
+        final Button buttonsolar18 = findViewById(R.id.buttonsolar18);
+        final Button buttonsolar19 = findViewById(R.id.buttonsolar19);
+        final Button buttonsolar20 = findViewById(R.id.buttonsolar20);
+        final Button buttonsolar21 = findViewById(R.id.buttonsolar21);
+        final Button buttonsolar22 = findViewById(R.id.buttonsolar22);
+
         final FloatingActionButton back = findViewById(R.id.back);
+
+        final Button[] buttons = new Button[22];
+        buttons[0] = buttonsolar1;
+        buttons[1] = buttonsolar2;
+        buttons[2] = buttonsolar3;
+        buttons[3] = buttonsolar4;
+        buttons[4] = buttonsolar5;
+        buttons[5] = buttonsolar6;
+        buttons[6] = buttonsolar7;
+        buttons[7] = buttonsolar8;
+        buttons[8] = buttonsolar9;
+        buttons[9] = buttonsolar10;
+        buttons[10] = buttonsolar11;
+        buttons[11] = buttonsolar12;
+        buttons[12] = buttonsolar13;
+        buttons[13] = buttonsolar14;
+        buttons[14] = buttonsolar15;
+        buttons[15] = buttonsolar16;
+        buttons[16] = buttonsolar17;
+        buttons[17] = buttonsolar18;
+        buttons[18] = buttonsolar19;
+        buttons[19] = buttonsolar20;
+        buttons[20] = buttonsolar21;
+        buttons[21] = buttonsolar22;
 
         //SET EVENTO
         ObjectAnimator fadeInBack = ObjectAnimator.ofFloat(back, "translationX", 0f);
         fadeInBack.setDuration(250);
         fadeInBack.start();
 
-        button.setOnClickListener(new View.OnClickListener() {
+        buttonsolar1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                moveMap(250f,-400f,1.7f);
-                hideButton(button);
+                int[] location = new int[2];
+                buttonsolar1.getLocationInWindow(location);
+                int x = location[0] + view.getWidth() / 2;
+                int y = location[1] + view.getHeight() / 2;
+                moveMap(x*-3.5f+2100,y*-3.5f+1300,3.5f);
+                hideButtons(buttons);
                 final FloatingActionButton back = findViewById(R.id.back);
                 back.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
                         resetMap();
-                        showButton(button);
+                        showButtons(buttons);
                         setBackAsBack();
                     }
                 });
             }
         });
+
+        buttonsolar16.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int[] location = new int[2];
+                buttonsolar16.getLocationInWindow(location);
+                int x = location[0] + view.getWidth() / 2;
+                int y = location[1] + view.getHeight() / 2;
+                moveMap(x*-3.5f+2100,y*-3.5f+1300,3.5f);
+                hideButtons(buttons);
+                final FloatingActionButton back = findViewById(R.id.back);
+                back.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        resetMap();
+                        showButtons(buttons);
+                        setBackAsBack();
+                    }
+                });
+            }
+        });
+
+
+
+
+
+
     }
 
     private void moveMap(float newX, float newY, float newScale){
@@ -93,8 +168,20 @@ public class MapNavigation extends AppCompatActivity {
         });
     }
 
+    private void hideButtons(Button[] buttons) {
+        for (int i = 0;i < buttons.length;i++) {
+            hideButton(buttons[i]);
+        }
+    }
+
+    private void showButtons(Button[] buttons) {
+        for (int i = 0;i < buttons.length;i++) {
+            showButton(buttons[i]);
+        }
+    }
+
     private void hideButton(Button button){
-        ObjectAnimator fadeOut = ObjectAnimator.ofFloat(findViewById(R.id.button),"alpha",0f);
+        ObjectAnimator fadeOut = ObjectAnimator.ofFloat(findViewById(button.getId()),"alpha",0f);
         fadeOut.setDuration(100);
         fadeOut.start();
         button.setEnabled(false);
